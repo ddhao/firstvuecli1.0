b31d10c2ed4b4103bdb6d7afed8c26904941854f
ddhao <603416005@qq.com>
2020-11-02 17:24:43 +0800

    自适应、css嵌套、cssnext配置与安装

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'reset.css' // 将默认的html标签样式清除
 
 createApp(App).use(store).use(router).mount('#app')
